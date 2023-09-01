dfb38a490d3b5cb7b48191c56cbbca9e6370f62b
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2023-09-01 15:19:24 +0600

    Time: 39 ms (82.46%), Space: 28.2 MB (73.45%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -9,39 +9,25 @@ public:
             if (a[i]%3 == 1) vp1.push_back({a[i]%3, a[i]});
             if (a[i]%3 == 2) vp2.push_back({a[i]%3, a[i]});
         }
+        vp1.push_back({1,1e5});
+        vp1.push_back({1,1e5});
+        vp2.push_back({2,1e5});
+        vp2.push_back({2,1e5});
         sort(vp1.begin(),vp1.end());
         sort(vp2.begin(),vp2.end());
         int x = sm%3;
         cout << x << endl;
         if (x == 1)
         {
-            int x = 0;
-            if (vp2.size() >= 2) {
-                x += vp2[0].second;
-                x += vp2[1].second;
-            }
-            if (vp1.size()) {
-                if (x) {
-                    sm -= min(x, vp1[0].second);
-                }
-                else sm -= vp1[0].second;
-            }
-            else sm -= x;
+            int x = vp1[0].second;
+            int y = vp2[0].second + vp2[1].second;
+            sm -= min(x,y);
         }
         else if (x == 2)
         {
-            int x = 0; 
-            if (vp1.size() >= 2) {
-                x += vp1[0].second;
-                x += vp1[1].second;
-            } 
-            if (vp2.size()) {
-                if (x) {
-                    sm -= min(x, vp2[0].second);
-                }
-                else sm -= vp2[0].second;
-            }
-            else sm -= x;
+            int x = vp2[0].second;
+            int y = vp1[0].second + vp1[1].second;
+            sm -= min (x,y);
         }
         return sm;
     }
