fb66dcfa3b4b73dcfcce69384b34646b384b711d
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2023-09-22 14:58:43 +0600

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -3,43 +3,10 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
     public:
-    // int left(int a[], int n, int x)
-    // {
-    //     int ans = -1, l = 0, r = n-1;
-    //     while(l <= r)
-    //     {
-    //         int mid = (l+r)/2;
-    //         if (a[mid] == x) {
-    //             ans = mid;
-    //             r = mid-1;
-    //         }
-    //         else if (a[mid] > x) {
-    //             r = mid-1;
-    //         }
-    //         else l = mid+1;
-    //     }
-    //     return ans;
-    // }
-    // int right(int a[], int n, int x)
-    // {
-    //     int ans = -1, l = 0, r = n-1;
-    //     while(l <= r)
-    //     {
-    //         int mid = (l+r)/2;
-    //         if (a[mid] == x) {
-    //             ans = mid;
-    //             l = mid+1;
-    //         }
-    //         else if (a[mid] > x) {
-    //             r = mid-1;
-    //         }
-    //         else l = mid+1;
-    //     }
-    //     return ans;
-    // }
     int left(int a[], int n, int x)
     {
         int ans = -1, l = 0, r = n-1;
@@ -50,7 +17,9 @@ class Solution
                 ans = mid;
                 r = mid-1;
             }
-            else if (a[mid] > x) r = mid-1;
+            else if (a[mid] > x) {
+                r = mid-1;
+            }
             else l = mid+1;
         }
         return ans;
@@ -65,7 +34,9 @@ class Solution
                 ans = mid;
                 l = mid+1;
             }
-            else if (a[mid] > x) r = mid-1;
+            else if (a[mid] > x) {
+                r = mid-1;
+            }
             else l = mid+1;
         }
         return ans;
