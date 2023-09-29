e62c02fc043ea35105a39d48168a9a8d18da5700
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2023-09-29 16:37:40 +0600

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -12,19 +12,18 @@ class Solution {
   public:
     int dx[5] = {1,-1,0,0};
     int dy[5] = {0,0,1,-1};
-    int n, m, ans = 0, c = 0, f = 0;
-    void dfs(vector<vector<int>>&a, int i, int j, vector<vector<int>>&vis)
+    int n, m, ans = 0, f = 0;
+    void dfs(vector<vector<int>>&a, int i, int j, int &c, vector<vector<int>>&vis)
     {
         if (!i or !j or i == n-1 or j == m-1) f = 1;
         c++;
-        //cout << i << " " << j << " " << c << " " << f << endl;
         vis[i][j] = 1;
         for (int k = 0; k < 4; k++) 
         {
             int x = dx[k]+i, y = dy[k]+j;
             if (x < n and y < m and x >= 0 and y >= 0 and a[x][y] and !vis[x][y]) 
             {
-                dfs(a,x,y,vis);
+                dfs(a,x,y,c,vis);
             }
         }
     }
@@ -36,12 +35,11 @@ class Solution {
             for (int j = 0; j < m; j++) {
                 if (a[i][j] and !vis[i][j])
                 {
-                    dfs(a,i,j,vis);
-                    //cout << c << endl;
+                    int c = 0;
+                    dfs(a,i,j,c,vis);
                     if (!f) {
                         ans += c;
                     }
-                    c = 0;
                     f = 0;
                 }
             }
