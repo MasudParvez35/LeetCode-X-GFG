6df6a09c5e3246c68a7ff9fd76baa3609d6eef84
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2023-10-02 15:16:43 +0600

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -5,16 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
   public:
-    int dfs(int a[], int n, int i, int sm, vector<vector<int>>&dp)
+    // int dfs(int a[], int n, int i, int sm, vector<vector<int>>&dp)
+    // {
+    //     if (sm == 0) return 0;
+    //     if (i < 0) return sm;
+    //     if (dp[i][sm] != -1) return dp[i][sm];
+    //     int ntake = dfs(a,n,i-1,sm,dp), take = 1e9;
+    //     if (a[i] <= sm) {
+    //         take = dfs(a,n,i-1,sm+a[i],dp);
+    //     }
+    //     return dp[i][sm] = min(take, ntake);
+    // }
+    int dfs(int a[], int n, int i, int sm, int t, vector<vector<int>>&dp)
     {
-        if (sm == 0) return 0;
+        if (sm == t) return sm;
         if (i < 0) return sm;
         if (dp[i][sm] != -1) return dp[i][sm];
-        int ntake = dfs(a,n,i-1,sm,dp), take = 1e9;
-        if (a[i] <= sm) {
-            take = dfs(a,n,i-1,sm-a[i],dp);
+        int ntake = dfs(a,n,i-1,sm,t,dp), take = 0;
+        if (sm+a[i] <= t) {
+            take = dfs(a,n,i-1,sm+a[i],t,dp);
         }
-        return dp[i][sm] = min(take, ntake);
+        return dp[i][sm] = max(take, ntake);
     }
 	int minDifference(int a[], int n) 
 	{ 
@@ -22,9 +33,8 @@ class Solution{
 	    for (int i = 0; i < n; i++) sm += a[i];
 	    int x = (sm+1)/2;
 	    vector<vector<int>> dp(n+1, vector<int>(x+1,-1));
-	    int p = dfs(a,n,n-1,x,dp);
-	    x -= p;
-	    return abs(sm - 2*x);
+	    int p = dfs(a,n,n-1,0,x,dp);
+	    return abs(sm - 2*p);
 	} 
 };
 
