f9658f7543382938fb566302aa9a19c235018e4f
Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-02-09 11:09:02 +0600

    Time: 121 ms (80.95%), Space: 46.6 MB (71.43%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public IList<int> LargestDivisibleSubset(int[] a) 
+    {
+        List<int> ans = new List<int>();
+        Array.Sort(a);
+        int mx = 0, val = a[0], n = a.Length;
+        int[] dp = new int[n+1];
+        for (int i = 1; i < a.Length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[i] % a[j] == 0) {
+                    dp[i] = Math.Max(dp[i], dp[j]+1);
+                    if (dp[i] > mx) {
+                        mx = dp[i];
+                        val = a[i];
+                    }
+                }
+            }
+        }
+        for (int i = n-1; i >= 0; i--)
+        {
+            if (dp[i] == mx && val % a[i] == 0)
+            {
+                ans.Add(a[i]);
+                mx--;
+                val = a[i];
+            }
+        }
+        if (ans.Count() == 0) ans.Add(a[0]);
+        return ans;
+    }
+}
