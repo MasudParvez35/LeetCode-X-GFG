65607f8f4e279c6ad7aeef44555ae0ad64500eee
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-15 08:55:15 +0600

    Time: 0 ms (100%), Space: 14.3 MB (16.18%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void dfs(vector<int>&a, vector<int>v, int i, int n)
+    {
+        if (i >= n) {
+            ans.push_back(v);
+            return;
+        }
+        dfs(a,v,i+1,n);
+        v.push_back(a[i]);
+        dfs(a,v,i+1,n);
+    }
+    vector<vector<int>> subsets(vector<int>& a) 
+    {
+        int n = a.size();
+        vector <int> v;
+        dfs(a,v,0,n);
+        return ans;
+    }
+};
