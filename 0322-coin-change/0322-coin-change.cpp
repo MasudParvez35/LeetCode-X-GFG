d047e4865d87a6f40104233fdc07e4cfad1eff59
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-18 12:54:22 +0600

    Time: 44 ms (40.03%), Space: 51.5 MB (11.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int dp[15][10005];
-    int dfs(vector<int>&a, int i, int t)
+    int dfs(vector<int>&a, int i, int t, vector<vector<int>>&dp)
     {
         if (i < 0 || t < 0) return 1e9;
         if (i == 0) {
             if (t % a[i] == 0) return t/a[i];
-            else  return 1e9;
+            else return 1e9;
         }
         if (dp[i][t] != -1) return dp[i][t];
-        int take = 1 + dfs(a, i, t-a[i]);
-        int ntake = dfs(a,i-1,t);
+        int take = 1 + dfs(a, i, t-a[i], dp);
+        int ntake = dfs(a, i-1, t, dp);
         return dp[i][t] = min(take, ntake);
     }
     int coinChange(vector<int>& a, int t) 
     {
-        memset(dp, -1, sizeof(dp));
         int n = a.size();
-        int ans = dfs(a,n-1,t);
+        vector<vector<int>> dp(n+1, vector<int>(t+1, -1));
+        int ans = dfs(a,n-1,t,dp);
         return ans >= 1e9 ? -1 : ans;
     }
 };
