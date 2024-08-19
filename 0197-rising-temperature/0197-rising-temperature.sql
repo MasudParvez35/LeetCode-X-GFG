f6aa71646ff2ba55e9dad4b712ca46995c5dfc89
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-19 08:49:17 +0600

    Time: 436 ms (69.79%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select w2.id
 from Weather w1 join Weather w2
-on date_add(w1.recordDate, interval 1 day) = w2.recordDate
+on DATEADD(day, 1, w1.recordDate) = w2.recordDate
 where w2.temperature > w1.temperature;
