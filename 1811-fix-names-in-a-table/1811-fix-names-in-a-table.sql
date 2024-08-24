e2ac9e2a15988bb3ea155fdb748322963089448e
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-24 11:11:28 +0600

    Time: 861 ms (95.86%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select user_id,
-concat(upper(substring(name,1,1)), lower(substring(name,2,length(name)-1))) name
+concat(upper(substring(name,1,1)), lower(substring(name,2,len(name)-1))) name
 from users
 order by user_id;
