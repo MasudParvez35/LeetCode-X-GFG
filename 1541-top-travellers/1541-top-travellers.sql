08a5fd895fc9b1371c24d813221cd6ec90aa92bb
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-24 06:32:23 +0600

    Time: 587 ms (98.79%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
-select u.name, COALESCE(Sum(r.distance),0) as travelled_distance
+/* Write your T-SQL query statement below */
+select u.name, ISNULL(Sum(r.distance),0) as travelled_distance
 from Users u left join Rides r 
 on u.id = r.user_id
 group by u.id, u.name
-order by travelled_distance desc, name asc;
+order by travelled_distance desc, u.name asc;
