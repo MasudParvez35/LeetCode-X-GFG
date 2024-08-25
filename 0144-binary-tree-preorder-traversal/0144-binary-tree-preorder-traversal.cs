05b5167b9a497202af263ea6745ac38f174ef9a6
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-25 08:49:25 +0600

    Time: 94 ms (78.8%), Space: 45.7 MB (43.68%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    void dfs(TreeNode root, List<int> ans)
+    {
+        if (root == null) 
+            return;
+        ans.Add(root.val);
+        dfs(root.left, ans);
+        dfs(root.right, ans);
+    }
+    public IList<int> PreorderTraversal(TreeNode root) 
+    {
+        List<int> ans = new List<int>();
+        dfs(root, ans);
+        return ans;
+    }
+}
