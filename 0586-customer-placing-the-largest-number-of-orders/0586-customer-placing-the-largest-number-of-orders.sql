115d4d0cf43acac22a78d7626090b2e7f7c9faa0
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-28 08:54:28 +0600

    Time: 469 ms (49.2%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,6 @@
-/* Write your T-SQL query statement below */
-select top 1 customer_number
-from Orders
+# Write your MySQL query statement below
+select customer_number
+from Orders 
 group by customer_number
-order by count(customer_number) desc;
+order by count(customer_number) desc
+limit 1;
