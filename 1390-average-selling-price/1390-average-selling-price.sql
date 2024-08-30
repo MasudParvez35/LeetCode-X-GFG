3b366a1a4bcb048c5cf37fd3330fc926bdb1d403
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-30 08:42:06 +0600

    Time: 514 ms (16.73%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,6 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT p.product_id, COALESCE(ROUND(SUM(p.price*u.units)*1.00/SUM(u.units),2),0) as average_price
-FROM Prices p
-LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id 
+SELECT p.product_id, COALESCE(ROUND(sum(price*units)*1.0/SUM(units),2),0) as average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND purchase_date BETWEEN start_date AND end_date
+group by p.product_id;
