3eb5ca645808265dd76c73380a9eb7fc9fdbc64f
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2024-08-31 11:11:04 +0600

    Time: 3218 ms (22.11%), Space: 0B (100%) - LeetHub

--- 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
 select p.product_id, product_name
 from Product p left join Sales s
 on p.product_id = s.product_id
-group by p.product_id
-having min(sale_date) >= '2019-01-01' and max(sale_date) <= '2019-03-31';
+group by p.product_id, p.product_name
+having min(sale_date) >= '2019-01-01' and max(sale_date) <= '2019-03-31'
+order by p.product_id
