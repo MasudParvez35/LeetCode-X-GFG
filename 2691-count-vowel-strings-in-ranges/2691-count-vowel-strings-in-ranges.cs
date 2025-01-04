499fcf90a2f862fdc7f25713251248c74e28faf7
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2025-01-04 23:58:46 +0600

    Time: 3 ms (80.31%), Space: 83.6 MB (38%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cs
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cs
@@ -1,37 +1,20 @@
 public class Solution {
-    public bool IsVowel(char c) {
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-            return true;
-        else 
-            return false;
-    }
-    public bool Check(string s)
-    {
-        if (IsVowel(s[0]) && IsVowel(s[s.Length-1]))
-            return true;
-        else 
-            return false;
-    }
     public int[] VowelStrings(string[] s, int[][] q) 
     {
         int[] pre = new int[s.Length+1];
         int[] ans = new int[q.Length];
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (Check(s[i]) == true)
-                pre[i] = 1;
-            if (i > 0)
-                pre[i] += pre[i-1];
+        for (int i = 0; i < s.Length; i++) {
+            pre[i + 1] = pre[i] + (IsVowel(s[i][0]) && IsVowel(s[i][s[i].Length - 1]) ? 1 : 0);
         }
         for (int i = 0; i < q.Length; i++)
         {
             int left = q[i][0];
             int right = q[i][1];
-            if (left == 0)
-                ans[i] = pre[right];
-            else 
-                ans[i] = pre[right] - pre[left-1];
+            ans[i] = pre[right+1] - pre[left];
         }
         return ans;
+    }
+    private bool IsVowel(char c) {
+        return "aeiou".Contains(c);
     }
 }
