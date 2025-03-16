15851c3c38b9a974214a62dfd33ed934b58e5ee5
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2025-03-16 20:24:19 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public long RepairCars(int[] ranks, int cars) 
+    {
+        long ans = int.MaxValue;
+        long left = 1, right = int.MaxValue;
+        while(left <= right)
+        {
+            long mid = (left + right) / 2, count = 0;
+            for (int ind = 0; ind < ranks.Length; ind++)
+            {
+                long x = (long)Math.Sqrt(mid/(double)ranks[ind]);
+                count += x;
+            }
+            if (count >= cars)
+            {
+                ans = mid;
+                right = mid - 1;
+            }
+            else 
+                left = mid + 1;
+        }
+        
+        return ans;
+    }
+}
