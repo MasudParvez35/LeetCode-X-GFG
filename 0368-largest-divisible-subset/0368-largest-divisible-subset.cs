a0fb5418fb2326914aa122593b7b1edaee0bf7cf
Masud Parvez <58568171+MasudParvez35@users.noreply.github.com>
2025-04-06 10:12:52 +0600

    Time: 16 ms (87.5%), Space: 47.6 MB (62.5%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cs
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cs
@@ -5,17 +5,23 @@ public class Solution {
         Array.Sort(a);
         int mx = 0, val = a[0], n = a.Length;
         int[] dp = new int[n+1];
-        for (int i = 1; i < a.Length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (a[i] % a[j] == 0) {
+
+        for (int i = 1; i < a.Length; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                if (a[i] % a[j] == 0) 
+                {
                     dp[i] = Math.Max(dp[i], dp[j]+1);
-                    if (dp[i] > mx) {
+                    if (dp[i] > mx) 
+                    {
                         mx = dp[i];
                         val = a[i];
                     }
                 }
             }
         }
+
         for (int i = n-1; i >= 0; i--)
         {
             if (dp[i] == mx && val % a[i] == 0)
@@ -25,6 +31,7 @@ public class Solution {
                 val = a[i];
             }
         }
+        
         if (ans.Count() == 0) ans.Add(a[0]);
         return ans;
     }
